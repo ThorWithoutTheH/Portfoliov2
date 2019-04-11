a036bd3248cd751b1a4c59ac908893c2a53cd6ff
Tor Rosa <vrosa2@my.bcit.ca>
2019-04-11 14:00:12 -0700

    (feature): ear trainer
    added ear trainer to projects, cleaned up some of the text spacing for projects.

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -4,17 +4,30 @@ import history from "../history";
 
 import ProjectBattlefy from "../assets/images/ProjectBattlefy.jpg";
 import ProjectGameJam18 from "../assets/images/ProjectGameJam18.jpg";
-import ProjectEarTrainer from "../assets/images/ProjectSnippet.png";
+import ProjectEarTrainer from "../assets/images/ProjectEarTrainer.jpg";
 import ProjectFlyCasual from "../assets/images/ProjectFlyCasual.jpg";
 
 const ProjectCarouselWrapper = styled.div`
-  margin-top: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: flex-start;
 `;
 
 const Title = styled.h1`
+  margin-top: 300px;
   font-family: Raleway;
   justify-content: center;
   text-align: center;
+`;
+
+const ImageWrap = styled.div`
+  text-align: center;
+  transition: transform 300ms ease-in-out;
+
+  &:hover {
+    transform: translate(0px, -15px);
+  }
 `;
 
 const Project1Snippet = styled.img`
@@ -22,9 +35,12 @@ const Project1Snippet = styled.img`
   transition: transform 300ms ease-in-out;
   cursor: pointer;
 
-  &:hover {
-    transform: translate(0px, -15px);
-  }
+`;
+
+const TextForProject = styled.div`
+  font-family: Abel-Regular;
+  font-size: 22px;
+  height: 30px;
 `;
 
 const Project2Snippet = styled.img`
@@ -32,9 +48,6 @@ const Project2Snippet = styled.img`
   transition: transform 300ms ease-in-out;
   cursor: pointer;
 
-  &:hover {
-    transform: translate(0px, -15px);
-  }
 `;
 
 const Project3Snippet = styled.img`
@@ -42,9 +55,6 @@ const Project3Snippet = styled.img`
   transition: transform 300ms ease-in-out;
   cursor: pointer;
 
-  &:hover {
-    transform: translate(0px, -15px);
-  }
 `;
 
 const Project4Snippet = styled.img`
@@ -52,9 +62,6 @@ const Project4Snippet = styled.img`
   transition: transform 300ms ease-in-out;
   cursor: pointer;
 
-  &:hover {
-    transform: translate(0px, -15px);
-  }
 `;
 
 const ProjectText = styled.div`
@@ -81,13 +88,27 @@ function toFlyCasual() {
 
 function ProjectCarousel() {
   return (
-    <ProjectCarouselWrapper>
+    <div>
       <Title>Projects & Featured Work</Title>
-      <Project1Snippet src={ProjectBattlefy} onClick={toBattlefy} />
-      <Project2Snippet src={ProjectGameJam18} onClick={toGameJam} />
-      <Project3Snippet src={ProjectEarTrainer} onClick={toEarTrainer} />
-      <Project4Snippet src={ProjectFlyCasual} onClick={toFlyCasual} />
-    </ProjectCarouselWrapper>
+      <ProjectCarouselWrapper>
+        <ImageWrap>
+          <Project1Snippet src={ProjectBattlefy} onClick={toBattlefy} />
+          <TextForProject>Battlefy: SSBU Open Nintendo of America</TextForProject>
+        </ImageWrap>
+        <ImageWrap>
+          <Project2Snippet src={ProjectGameJam18} onClick={toGameJam} />
+          <TextForProject>BC Game Jam 18: First Place</TextForProject>
+        </ImageWrap>
+        <ImageWrap>
+        <Project3Snippet src={ProjectEarTrainer} onClick={toEarTrainer} />
+        <TextForProject>Ear Trainer - iOS App</TextForProject>
+        </ImageWrap>
+        <ImageWrap>
+        <Project4Snippet src={ProjectFlyCasual} onClick={toFlyCasual} />
+        <TextForProject>Fly Casual - Open Source Game</TextForProject>
+        </ImageWrap>
+      </ProjectCarouselWrapper>
+    </div>
   );
 }
 
