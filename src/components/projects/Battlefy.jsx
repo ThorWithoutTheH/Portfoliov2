5a2de9aa79419c1d02154543b5bacec9106df0ed
Tor Rosa <vrosa2@my.bcit.ca>
2019-04-12 15:54:40 -0700

    feature(projectbfy)
    Updated projects pages with content.... yes. actual content.

--- src/components/projects/Battlefy.jsx
+++ src/components/projects/Battlefy.jsx
@@ -27,13 +27,23 @@ const ProjectTitleAndAboutWrapper = styled.div`
   margin-bottom: 3em;
 `;
 
+const ProjectDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-size: cover;
+  width: 90%;
+  background-position: 50%;
+  margin-bottom: 3em;
+
+`;
+
 const HeaderStyled = styled.header`
   margin: auto 5% auto 0;
   max-width: 640px;
 `;
 
 const ProjectTitle = styled.h1`
-  font-family: Raleway;
+  font-family: Abel-Regular;
   font-size: 22px;
   letter-spacing: 3px;
 
@@ -45,34 +55,47 @@ const ProjectAboutBold = styled.h2`
 
 `;
 
-// TODO: MAKE THIS LARGER THAN VIEW PORT 
-const ProjectScreenShot = styled.div`
+const SmashPortalScreen = styled.div`
   width: 100vw;
+  height: 500px;
   background: url(${BfProject}) no-repeat center;
   background-size: contain;
   margin-bottom: 1.5em;
   position: relative;
   left: 50%;
   right: 50%;
-  margin-left: 50vw;
+  margin-left: -50vw;
   margin-right: -50vw;  
 `;  
-
-const ProjectCenterText = styled.div`
-
-
-
-`;
-
-const LeftTextLinkToProject = styled.a`
-
+// TODO: Add Another Screenshot of the different regions
+const SmashRegionScreen = styled.div`
 
 
 
 
 `;
 
+const ProjectDetails = styled.p`
+  font-family: Raleway;
+  width: 50%;
 
+`;
+
+const Bullets = styled.ul`
+`;
+
+const ProjectBullet = styled.li`
+  font-family: Raleway;
+  width: 50%; 
+`;
+
+const LinkToBattlefy = styled.a`
+
+
+
+`;
+
+// Navigating Projects
 const ProjectsWrapper = styled.div`
   max-width: 100%;
   display: flex;
@@ -126,6 +149,7 @@ const PreviousProj = styled.div`
  }
 `;
 
+
 function redirectBackToHome() {
   history.push('/');
 }
@@ -134,19 +158,42 @@ function toNextProj() {
   history.push('/projects-bcgamejam18') 
 }
 
-
+// TODO: Nav needs different functionality in projects 
 function Battlefy() {
   return ( 
   <ProjectsContainer>
     <Nav />
+    
     <Logo />
+
     <ProjectTitleAndAboutWrapper>
       <HeaderStyled>
-        <ProjectTitle>Super Smash Brothers Ultimate. Hosted by Battlefy</ProjectTitle>
+        <ProjectTitle>Super Smash Brothers Ultimate. Powered by Battlefy</ProjectTitle>
         <ProjectAboutBold>The largest Super Smash Bros tournament to date, with over 10000 entrants. The finalist were invited to PAX East. </ProjectAboutBold>
       </HeaderStyled>
     </ProjectTitleAndAboutWrapper>
-    <ProjectScreenShot />
+
+    <SmashPortalScreen />
+
+    <ProjectDetailsWrapper>
+      <ProjectTitle>Battlefy Goals</ProjectTitle>
+      <ProjectDetails>At Battlefy my role ran the spectrum from solving UX problems, and full stack engineering. All of that work is aimed at improving the tournament experience for our users while keeping our KPI's in mind.</ProjectDetails>
+      <Bullets>
+        <ProjectBullet>Help grow and cement the future of esports.</ProjectBullet>
+        <ProjectBullet>Create the best possible platform for organizers and ssers.</ProjectBullet>
+        <ProjectBullet>Develop and productise features for Battlefy.com.</ProjectBullet>
+        <ProjectBullet>Work with developers to create amazing tournaments.</ProjectBullet>
+      </Bullets>
+    </ProjectDetailsWrapper>
+
+    <SmashPortalScreen />
+
+    <ProjectDetailsWrapper>
+      <ProjectDetails>Portals are one of the most common entry points for our users. </ProjectDetails>
+    </ProjectDetailsWrapper>
+
+    <LinkToBattlefy>Battlefy Super Smash Bros 2019 Portal</LinkToBattlefy>
+    
     <ProjectsWrapper>
 		  <PreviousProj onClick={redirectBackToHome}>Return Home</PreviousProj>
 		  <NextProj onClick={toNextProj} >Next Project</NextProj>
